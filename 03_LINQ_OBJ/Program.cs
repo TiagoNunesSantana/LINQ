2613a54a7f8f6d32f0bc6e00338a3c3cc322606d
TiagoSantana <tiagonunessantana87@gmail.com>
2020-06-12 12:30:22 -0300

    Objectos no LINQ

---
+++ 03_LINQ_OBJ/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_LINQ_OBJ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Usuario> lista = new List<Usuario>();
+            lista.Add(new Usuario() { Nome = "Tiago", Email="tiago@gmail.com"});
+            lista.Add(new Usuario() { Nome = "Nani", Email = "nani@hotmail.com" });
+            lista.Add(new Usuario() { Nome = "Alice", Email = "alice@ig.com" });
+            lista.Add(new Usuario() { Nome = "Pai", Email = "pai@gmail.com" });
+            lista.Add(new Usuario() { Nome = "Mãe", Email = "mae@gmail.com" });
+
+            var ListaFiltrada = lista.Where(a => a.Email.Contains("@gmail.com")).OrderBy(a=>a.Nome).Select(a => a);
+
+            foreach (var item in ListaFiltrada)
+            {
+                Console.WriteLine(item.Nome + " - " + item.Email);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
